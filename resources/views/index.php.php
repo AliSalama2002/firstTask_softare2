b45318ab6d033fd1f80caa0a42071b7767af2631
AliSalama2002 <gamy16981@gmail.com>
2023-04-17 13:10:29 +0200

    added file

---
+++ resources/views/index.php.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student name</title>
+</head>
+<style>
+</style>
+<body style="background-color:aliceblue;">
+<table border="1"
+ style="border: 1px solid #ccc;
+  font-size:larger;
+  width:100%">
+  <caption style="font-size: 150%; ">Students Name</caption>
+    <thead>
+        <th> Name</th>
+        <th> Sec</th>
+        <th> Depart</th>
+        <th> Email</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Ali salama</td>
+            <td> 8</td>
+            <td>CS</td>
+            <td>salama22@gmail</td>
+        </tr>
+        <tr>
+            <td>Ahmed salama</td>
+            <td> 2</td>
+            <td>CS</td>
+            <td>ahmed285@gmail</td>
+        </tr>
+        <tr>
+            <td>Mostafa salama</td>
+            <td> 9</td>
+            <td>IS</td>
+            <td>ms2009@gmail</td>
+        </tr>
+        <tr>
+            <td>mohamed salama</td>
+            <td> 9</td>
+            <td>CS</td>
+            <td>moss05a@gmail</td>
+        </tr>
+        <tr>
+            <td>abdo adel</td>
+            <td> 8</td>
+            <td>CS</td>
+            <td>adel56@gmail</td>
+        </tr>
+        <tr>
+            <td>sara ahmed</td>
+            <td> 6</td>
+            <td>IT</td>
+            <td>saraa20@gmail</td>
+        </tr>
+        <tr>
+            <td>Reham mohamed</td>
+            <td> 7</td>
+            <td>CS</td>
+            <td>reh099@gmail</td>
+        </tr>
+        <tr>
+            <td>mahmoud emad</td>
+            <td> 10</td>
+            <td>CS</td>
+            <td>omdaa11@gmail</td>
+        </tr>
+        <tr>
+            <td>Elsaid ahmed</td>
+            <td> 3</td>
+            <td>IS</td>
+            <td>saaidAna@gmail</td>
+        </tr>
+        <tr>
+            <td>Abdo ahmed</td>
+            <td> 8</td>
+            <td>IT</td>
+            <td>abdo995@gmail</td>
+        </tr>
+        <tr>
+            <td>saif samir</td>
+            <td> 6</td>
+            <td>IT</td>
+            <td>saiiff52@gmail</td>
+        </tr>
+        <tr>
+            <td>nour ibrahem</td>
+            <td> 6</td>
+            <td>IS</td>
+            <td>nour33@gmail</td>
+        </tr>
+        <tr>
+            <td>Mona mahmoud</td>
+            <td> 9</td>
+            <td>CS</td>
+            <td>moMahmoud44@gmail</td>
+        </tr>
+        <tr>
+            <td>Ahmed mostafa</td>
+            <td> 2</td>
+            <td>IT</td>
+            <td>am99@gmail</td>
+        </tr>
+        <tr>
+            <td>mena ahmed</td>
+            <td> 9</td>
+            <td>CS</td>
+            <td>mema2000@gmail</td>
+        </tr>
+        <tr>
+            <td>Zeiad samir</td>
+            <td> 10</td>
+            <td>CS</td>
+            <td>zaid20@gmail</td>
+        </tr>
+        <tr>
+            <td>mariam mohamed</td>
+            <td> 9</td>
+            <td>CS</td>
+            <td>meroo30@gmail</td>
+        </tr>
+    </tbody>
+
+</table>
+
+</body>
+</html>
